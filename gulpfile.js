bd7bb7e34a53550a137a42744b2b11061cbc686d
Michael Brown <michaelfbrown.88@gmail.com>
2014-08-31 23:31:44 -0400

    fix gulp watch, edit font weights

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,21 @@
 var gulp = require('gulp'),
 sass = require('gulp-ruby-sass'),
-watch = require('gulp-watch'),
-livereload = require('gulp-livereload'),
-lr = require('tiny-lr'),
-server = lr();
-
+watch = require('gulp-watch');
 
 gulp.task('sass', function () {
     return gulp.src('scss/style.scss')
         .pipe(sass({sourcemap: true, sourcemapPath: './'}))
         .on('error', function (err) { console.log(err.message); })
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('./'));
 });
 
+gulp.task('watch', function () {
     watch({glob: 'scss/**/*.scss'}, function(files) {
         return files.pipe(sass({sourcemap: true, sourcemapPath: './'}))
             .pipe(gulp.dest('./'));
-            
     });
 });
+
 
 var watcher = gulp.watch('scss/**/*.scss', ['sass', 'watch']);
 
